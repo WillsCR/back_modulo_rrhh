7199381a6576b3b2672a583b1a73214637490e6e
WillsCR <102868946+WillsCR@users.noreply.github.com>
2025-11-06 11:53:06 -0300

    change auth.dto.ts

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -13,25 +13,10 @@ export class RegisterDto {
     @IsString()
     rut: string;
 
-    @IsString()
-    nombre: string;
-
-    @IsString()
-    apellido: string;
-
     @IsEmail({}, { message: 'El email debe tener un formato válido' })
     email: string;
 
     @IsString()
     @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
     password: string;
-
-    @IsString()
-    telefono?: string;
-
-    @IsString()
-    rol: string;
-
-    @IsNumber()
-    id_departamento: number;
 }
